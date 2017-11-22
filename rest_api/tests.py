26e3158b846743bee5f5f70014920fdf4db7f967
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2017-11-22 13:10:10 +0700

    added ability to retrieve, update, and delete

--- rest_api/tests.py
+++ rest_api/tests.py
@@ -33,3 +33,40 @@ class ViewTestCase(TestCase):
 
     def test_api_can_create_a_bucketlist(self):
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_api_can_get_a_bucketlist(self):
+        bucketlist = Bucketlist.objects.get()
+        response = self.client.get(
+            reverse(
+                viewname='details',
+                kwargs={'pk': bucketlist.id}
+            ),
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, bucketlist)
+
+    def test_api_can_update_a_bucketlist(self):
+        bucketlist = Bucketlist.objects.get()
+        change_bucketlist = {'name': 'Something newer'}
+        response = self.client.put(
+            reverse(
+                viewname='details',
+                kwargs={'pk': bucketlist.id},
+            ),
+            data=change_bucketlist,
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_api_can_delete_a_bucketlist(self):
+        bucketlist = Bucketlist.objects.get()
+        response = self.client.delete(
+            reverse(
+                viewname='details',
+                kwargs={'pk': bucketlist.id},
+            ),
+            format='json',
+            follow=True
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
